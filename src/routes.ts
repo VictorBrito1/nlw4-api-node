05977fbd4f9ea8e4bc3260c9ceaf484dab6c33b4
Victor Brito <victor_britosoares@hotmail.com>
2021-02-26 12:06:10 -0300

    Answer controller

--- src/routes.ts
+++ src/routes.ts
@@ -2,18 +2,22 @@ import { Router } from 'express';
 import { SurveyController } from './controllers/SurveyController';
 import { UserController } from './controllers/UserController';
 import { SendMailController } from './controllers/SendMailController';
+import { AnswerController } from './controllers/AnswerController';
 
 const router = Router();
 
 const userController = new UserController();
 const surveyController = new SurveyController();
 const sendMailController = new SendMailController();
+const answerController = new AnswerController();
 
 router.post('/users', userController.create);
 
 router.post('/surveys', surveyController.create);
 router.get('/surveys', surveyController.index);
 
-router.post('/sendMail', sendMailController.execute)
+router.post('/sendMail', sendMailController.execute);
+
+router.get('/answers/:value', answerController.execute);
 
 export { router };
